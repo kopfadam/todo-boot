b3baa0ab6c977b361116e512feb0ba13870d62ab
Adam Kopf <adamkopf0@gmail.com>
2020-10-19 16:26:45 +0300

    created ToDo entity

---
+++ src/main/java/com/aga/todo/entity/ToDo.java
@@ -0,0 +1,61 @@
+package com.aga.todo.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+public class ToDo {
+
+    @Id
+    @NotNull
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int Id;
+    private String title;
+
+    @NotBlank
+    private String description;
+
+    @Column(updatable = false)
+    private LocalDateTime created;
+    private LocalDateTime modified;
+    private boolean completed = false;
+
+    public ToDo(String description){
+        this.description = description;
+    }
+
+    public ToDo(String title, String description){
+        this.title = title;
+        this.description = description;
+    }
+
+    @PrePersist
+    void onCreate(){
+        LocalDateTime date = LocalDateTime.now();
+        this.created = date;
+        this.modified = date;
+    }
+
+    @PreUpdate
+    void onUpdate(){
+        this.modified = LocalDateTime.now();
+    }
+
+    @Override
+    public String toString() {
+        return "ToDo{" +
+                "title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", created=" + created +
+                ", modified=" + modified +
+                ", completed=" + completed +
+                '}';
+    }
+}
