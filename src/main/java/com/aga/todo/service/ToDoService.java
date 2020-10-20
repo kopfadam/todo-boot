bcde98581d4c471130e8ad57194dd61ed794efca
Adam Kopf <adamkopf0@gmail.com>
2020-10-20 08:55:56 +0300

    created todo service class

---
+++ src/main/java/com/aga/todo/service/ToDoService.java
@@ -0,0 +1,54 @@
+package com.aga.todo.service;
+
+import com.aga.todo.entity.ToDo;
+import com.aga.todo.entity.User;
+import com.aga.todo.repository.ToDoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@Service
+@Transactional
+public class ToDoService {
+
+    @Autowired
+    private ToDoRepository toDoRepository;
+
+    public ToDo addToDo(ToDo toDo, User user){
+
+        toDo.setUser(user);
+        toDo = toDoRepository.save(toDo);
+
+        return toDo;
+    }
+
+    public ToDo getToDoById(int toDoId){
+
+        Optional<ToDo> toDo = toDoRepository.findById(toDoId);
+
+        if (!toDo.isPresent())
+            throw new RuntimeException("Nothing found");
+
+        return toDo.get();
+    }
+
+    public List<ToDo> getAllToDos(){
+        return toDoRepository.findAll();
+    }
+
+    public List<ToDo> getAllToDosByUser(User user){
+        return toDoRepository.findByUser(user);
+    }
+
+    public void delete(int todoId) {
+        toDoRepository.deleteById(todoId);
+    }
+
+    public ToDo update(ToDo toDo){
+        return toDoRepository.save(toDo);
+    }
+}
