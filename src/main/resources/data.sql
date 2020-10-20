3797766531722a3d5a3818b95db438a094107977
Adam Kopf <adamkopf0@gmail.com>
2020-10-20 10:10:59 +0300

    set up test and prod profiles

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into to_do (id,description,created,modified,completed)
+values ('1', 'Read a Book','2018-08-17 07:42:44.136','2018-08-17 07:42:44.137',true);
+insert into to_do (id,description,created,modified,completed)
+values ('2', 'Buy Movie Tickets','2018-08-17 09:50:10.126','2018-08-17 09:50:10.126',false);
+insert into to_do (id,description,created,modified,completed)
+values ('3', 'Clean my Room','2018-08-17 07:42:44.136','2018-08-17 07:42:44.137',false);
