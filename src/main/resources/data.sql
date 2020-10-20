93fa6306256fa2541f1babb6c7b59853da24368f
Adam Kopf <adamkopf0@gmail.com>
2020-10-20 11:25:29 +0300

    add sql queries to populate h2 database

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,13 @@
-insert into to_do (id,description,created,modified,completed)
-values ('1', 'Read a Book','2018-08-17 07:42:44.136','2018-08-17 07:42:44.137',true);
-insert into to_do (id,description,created,modified,completed)
-values ('2', 'Buy Movie Tickets','2018-08-17 09:50:10.126','2018-08-17 09:50:10.126',false);
-insert into to_do (id,description,created,modified,completed)
-values ('3', 'Clean my Room','2018-08-17 07:42:44.136','2018-08-17 07:42:44.137',false);
+insert into USER (id, created, email, modified, name, password)
+values ('1', '2018-08-17 07:42:44.136', 'as@mail.com', '2018-08-17 07:42:44.136', 'Aslan','pass');
+insert into USER (id, created, email, modified, name, password)
+values ('2', '2019-08-18 07:42:44.136', 'asd@mail.com', '2019-08-18 07:42:44.136', 'Kaplan','pass');
+insert into USER (id, created, email, modified, name, password)
+values ('3', '2020-08-19 07:42:44.136', 'asdf@mail.com', '2020-08-19 07:42:44.136', 'Fil','pass');
+
+
+
+insert into TODO (id,completed,created,description,modified,title,user_id)
+values ('1', false, '2018-08-17 07:42:44.136', 'Read a Book','2018-08-17 07:42:44.137', 'Read', '1');
+insert into TODO (id,completed,created,description,modified,title,user_id)
+values ('2', true, '2018-08-17 09:50:10.126', 'Pursuit of Happiness movie 2 tickets', '2018-08-17 09:50:10.126', 'Buy Movie Tickets', '1');
